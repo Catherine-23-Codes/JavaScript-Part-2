0e4de1b9963ed341a46458a0c921ec7b090e078e
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2026-01-09 20:04:10 +0530

    first commit

---
+++ ArrayMethods.js
@@ -0,0 +1,11 @@
+//Array Methods
+
+const myArr = [0, 1, 2, 4, 6, 7, 32, 9, 22];
+//1.Push()
+myArr.push(5); // this particular value within braces will be pushed inside the array
+myArr.push(37);
+console.log(myArr);
+
+//2.Pop()
+myArr.pop(2);
+console.log(myArr);
---
+++ Arrays.js
@@ -0,0 +1,11 @@
+//Array
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+//declaring an array 2 methods
+const myArray = [0, 1, 2, 3, 4, 5];
+console.log(myArray);
+const newArr = new Array(1, 5, 2, 2, 5, 9);
+console.log(newArr);
+
+// accessing an array elem - using index position
+console.log(myArray[3]);
